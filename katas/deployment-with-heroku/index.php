7f32953bec3a3843766ab5d802592a3ba47a05de
Fanny Cheung <fanny@ynote.hk>
2020-03-30 17:42:45 +0200

    Add plenary session resources on deployment with Heroku

---
+++ katas/deployment-with-heroku/index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Ada Tech School / Hello World</title>
+    <style>
+      html {
+        font-family: monospace, serif;
+      }
+
+      main {
+        padding: 1em 17em;
+      }
+
+      h1 {
+        text-align: center;
+      }
+    </style>
+  </head>
+
+  <body>
+    <main>
+      <h1>Hello World</h1>
+      <p>
+        Ceci est le nom de mon serveur : <?php echo $_SERVER['SERVER_NAME'] ?>.
+      </p>
+    </main>
+  </body>
+</html>
