c15eb0d8ea8c1b456b7e5dc5771da0eea426e0bb
Wara-nyu <w.bounkeua@gmail.com>
2020-07-02 12:26:11 +0200

    ajout du traitement des name et password

--- katas/mot-de-passe_service-en-ligne/process_data.py
+++ katas/mot-de-passe_service-en-ligne/process_data.py
@@ -1,7 +1,13 @@
 import hashlib
-
+import csv
 
 def data_process(name, password):
-        
-
+    h = hashlib.sha512()
+    password = bytes(str(password), "utf-8")
+    h.update(password)
+    with open("password.csv", "a") as file:
+        writer = csv.writer(file, delimiter=',')
+        writer.writerow([name, h.hexdigest()])
     return
+
+data_process("Nicola", "odnaondzvaonvan")
