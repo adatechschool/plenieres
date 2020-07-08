7d175eba154279d5a2c99790241087920e1311a3
KheOps <kheops@ceops.eu>
2020-07-08 20:40:48 +0200

    Ajout exercice de devinette d'affichage pour comprendre la base des fonctions
    Signed-off-by: KheOps <kheops@ceops.eu>

---
+++ katas/bases-sur-les-fonctions/devinette3.js
@@ -0,0 +1,13 @@
+function uneFonction(x, y) {
+  console.log ("Bonjour");
+  return
+  console.log ("Hello !");
+}
+
+var a = 2, b = 4, c = 1, d = 6, r;
+
+console.log ("Salut");
+
+r = uneFonction(a, b);
+
+console.log ("Coucou");
